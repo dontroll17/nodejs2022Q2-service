a732f0b48b181d6968570648fdbebe9c1630499e
dontroll17 <dontroll@yandex.ru>
2022-07-21 18:39:32 +0300

    feat: 422 code error

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -3,10 +3,13 @@ import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesEntity } from './entities/favs.entity';
+import { ArtistEntity } from 'src/artist/entities/artist.entity';
+import { AlbumEntity } from 'src/album/entities/album.entity';
+import { TrackEntity } from 'src/track/entities/track.entity';
 
 @Module({
   providers: [FavoritesService],
   controllers: [FavoritesController],
-  imports: [TypeOrmModule.forFeature([FavoritesEntity])]
+  imports: [TypeOrmModule.forFeature([FavoritesEntity, ArtistEntity, AlbumEntity, TrackEntity])]
 })
 export class FavoritesModule {}
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AlbumEntity } from 'src/album/entities/album.entity';
+import { ArtistEntity } from 'src/artist/entities/artist.entity';
+import { TrackEntity } from 'src/track/entities/track.entity';
 import { Repository } from 'typeorm';
 import { FavoritesEntity } from './entities/favs.entity';
 
@@ -7,7 +10,13 @@ import { FavoritesEntity } from './entities/favs.entity';
 export class FavoritesService {
   constructor(
     @InjectRepository(FavoritesEntity)
-    private favsRepository: Repository<FavoritesEntity>
+    private favsRepository: Repository<FavoritesEntity>,
+    @InjectRepository(TrackEntity)
+    private trackRepository: Repository<TrackEntity>,
+    @InjectRepository(AlbumEntity)
+    private albumRepository: Repository<AlbumEntity>,
+    @InjectRepository(ArtistEntity)
+    private artistRepository: Repository<ArtistEntity>
   ) {}
 
   
@@ -22,6 +31,11 @@ export class FavoritesService {
   }
 
   async addTrack(id: string) {
+    const track = await this.trackRepository.findOne({ where: { id }});
+
+    if(!track) {
+      throw new HttpException('id not found', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
     return `${id} added to favs`;
   }
 
@@ -30,6 +44,12 @@ export class FavoritesService {
   }
 
   async addAlbum(id: string) {
+    const album = await this.albumRepository.findOne({ where: { id }});
+
+    if(!album) {
+      throw new HttpException('id not found', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
     return `${id} added to favs`;
   }
 
@@ -38,6 +58,11 @@ export class FavoritesService {
   }
 
   async addArtist(id: string) {
+    const artist = await this.artistRepository.findOne({ where: { id }});
+
+    if(!artist) {
+      throw new HttpException('id not found', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
     return `${id} added to favs`;
   }
 
