065550aaa6279aac0f4b1526ba2ccdefbd74fc2a
root <root@ubuntu2004.linuxvmimages.local>
2022-07-31 21:55:55 +0300

    fix: fixed response status code

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -7,17 +7,19 @@ export class LoggerMiddleware implements NestMiddleware {
     constructor(private customLogger: customLogger) {}
 
     use(req: Request, res: Response, next: NextFunction ) {
-        const { ip, originalUrl, query, body} = req;
-        //res.statusCode always 200!
-        const { statusCode } = res;
 
-        this.customLogger.log(
-            `From ${ip}
-            Request url: ${originalUrl}
-            Request query: ${JSON.stringify(query)}
-            Request body: ${JSON.stringify(body)}
-            Response code: ${statusCode}`
-        );
+        res.on('finish', () => {
+            const { ip, originalUrl, query, body} = req;
+            const { statusCode } = res;
+    
+            this.customLogger.log(
+                `From ${ip}
+                Request url: ${originalUrl}
+                Request query: ${JSON.stringify(query)}
+                Request body: ${JSON.stringify(body)}
+                Response code: ${statusCode}`
+            );
+        });
         next();
     }
 }
